236e2945bbb45a06485facb0e27fcfa456287f1a
abhishek05061999 <160098428+abhishek05061999@users.noreply.github.com>
2024-03-07 23:45:37 +0530

    Add files via upload

---
+++ Activity.java
@@ -0,0 +1,29 @@
+package SoftwareDesign;
+
+public class Activity {
+	private String name;
+    private String description;
+    private double cost;
+    private int capacity;
+
+    public Activity(String name, String description, double cost, int capacity) {
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+        this.capacity = capacity;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+}
